89eb83e37fbaae79ef3b0c2ff3e9e358551e0208
Anderson Otavio dos Santos <andersonotavio@hotmail.com>
2025-06-05 14:57:36 -0300

    feat: atualiza os controllers com mock de dados

--- src/main/java/com/otavioweb/miniAlugelDeCarros/Carros/CarroController.java
+++ src/main/java/com/otavioweb/miniAlugelDeCarros/Carros/CarroController.java
@@ -1,12 +1,41 @@
 package com.otavioweb.miniAlugelDeCarros.Carros;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 public class CarroController {
   @GetMapping("/")
   public String welcome(){
     return "Welcome my app";
+  }
+
+  //Adicionar carro (CREATE)
+  @PostMapping("/adicionar-carro")
+  public String adicionar(){
+    return "Carro adicionado";
+  }
+  
+  //Mostrar todos os carros (READ)
+  @GetMapping("/carros")
+  public String allCarros(){
+    return "Todos os carros";
+  }
+
+  //Mostrar carro por ID (READ)
+  @GetMapping("/carro")
+  public String mostarCarroPorId(){
+    return "Mostar carro por Id";
+  }
+
+  //Atualizar carro (UPDATE)
+  @PutMapping("/alterar-carro")
+  public String alterarCarroPorId(){
+    return "Alterar carro por Id";
+  }
+
+  //Deletar carro (DELETE)
+  @DeleteMapping("/deletar-carro")
+  public String deletarCarroPorId(){
+    return "Deletar carro por Id";
   }
 }
--- src/main/java/com/otavioweb/miniAlugelDeCarros/Cliente/ClienteController.java
+++ src/main/java/com/otavioweb/miniAlugelDeCarros/Cliente/ClienteController.java
@@ -1,4 +1,38 @@
 package com.otavioweb.miniAlugelDeCarros.Cliente;
 
+import org.springframework.web.bind.annotation.*;
+
+@RestController
 public class ClienteController {
+
+
+  //Adicionar cliente (CREATE)
+  @PostMapping("/adicionar-cliente")
+  public String adicionar(){
+    return "Carro adicionado";
+  }
+
+  //Mostrar todos os cliente (READ)
+  @GetMapping("/clientes")
+  public String allClientes(){
+    return "Todos os carros";
+  }
+
+  //Mostrar cliente por ID (READ)
+  @GetMapping("/cliente")
+  public String mostarClientePorId(){
+    return "Mostar carro por Id";
+  }
+
+  //Atualizar cliente (UPDATE)
+  @PutMapping("/alterar-cliente")
+  public String alterarClientePorId(){
+    return "Alterar carro por Id";
+  }
+
+  //Deletar cliente (DELETE)
+  @DeleteMapping("deletar-cliente")
+  public String deletarClientePorId(){
+    return "Deletar carro por Id";
+  }
 }
